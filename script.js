8b47c4034b6b24d6901eb0f7da11d34c358d5b0d
Abdelmouiz <abdelmouiz_@outlook.fr>
2024-09-26 15:09:21 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Getting information
+let cards = document.querySelectorAll('.card');
+let resetButton = document.querySelector('.resetButton');
+
+// Button to do the reset function
+resetButton.onclick = function (){
+    reset();
+};
+
+// Adding Click Funcion to add Disabled Class to each card 
+cards.forEach (card =>
+    card.addEventListener("click", theChecker)
+);
+
+// Function to add Disabled Class to cards
+
+function theChecker(){
+    this.classList.add("disabled");
+};
+
+// Create Rest function 
+
+function reset(){
+    cards.forEach(card => card.classList.remove("disabled"))
+};
